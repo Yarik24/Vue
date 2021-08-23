4b9bc24812807a11db0c66ee4907c9262dbf1f4e
Yarik24 <jarslavkblv24@gmail.com>
2021-08-23 19:09:46 +0300

    дз 1

---
+++ dz 1/src/components/Calculator.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="calc">
+    {{ message }}
+    {{ message.length }}
+    {{ message.split().reverse().join() }}
+
+    <div>
+      <input
+        type="number"
+        placeholder="Operand1"
+        v-on:focus="onFocus"
+        v-model.number="operand1"
+      />
+      <input
+        type="number"
+        placeholder="Operand2"
+        @keyup="onKeyUp"
+        v-model.number="operand2"
+      />
+      = {{ result }}
+      <div class="keyboard">
+      <button @click="result = operand1 + operand2">+</button>
+      <button @click="div">-</button>
+      <button v-on:click="multi(operand1, operand2)">*</button>
+      <button v-on:click="pow(operand1, operand2)">^</button>
+      <button v-on:click="divide(operand1, operand2)">/</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data: () => ({
+    message: "Hello Vue",
+    operand1: 0,
+    operand2: 0,
+    result: 0,
+    array: [],
+  }),
+  methods: {
+    doThan(text, event) {
+      console.log(text, event);
+    },
+    div() {
+        this.result = this.operand1 - this.operand2
+    },
+    multi(op1, op2) {
+        this.result = op1 * op2
+    },
+    pow(o1, o2) {
+      console.log(this)
+      this.result = Math.pow(o1, o2)
+    },
+    divide(op1, op2) {
+      this.result = Math.round(op1/op2)
+    },
+    onFocus() {
+      console.log("onFocus");
+    },
+    onKeyUp() {
+      console.log("onKeyUp");
+    }
+  }
+};
+</script>
